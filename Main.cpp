ebf95fca497c3127b027f21b4092e72f64778a28
ToxSylph <yoguiwabj@gmail.com>
2024-07-12 04:27:31 -0600

    MP Bypass

--- Main.cpp
+++ Main.cpp
@@ -1,37 +1,35 @@
-#include <stdio.h>
 #include <windows.h>
-#include <TlHelp32.h>
 #include <Psapi.h>
-#include <iostream>
-#include <vector>
+#include <TlHelp32.h>
 #include <fstream>
-#include <string>
+#include <iostream>
+#include <sstream>
+#include "json.hpp"
+
+using json = nlohmann::json;
 
 // Win API
-HANDLE _hProcess;
-uintptr_t _moduleBase;
-DWORD _mSize = 0;
+HANDLE hProcess;
+HANDLE hConsole;
+uintptr_t moduleBase;
+DWORD mSize = 0;
 
-// Pointers
-uintptr_t _fowtoggleaddr;
-uintptr_t _fowmodeaddr;
-uintptr_t _jumppatchaddr;
-BYTE* buffer;
+bool run = false;
+bool active = false;
+bool patched = false;
 
-// Vector with offsets loaded from .txt
-std::vector<unsigned int> bases;
+int toggleKey = 0x50; // P Key
+int exitKey = VK_END; // End Key
+bool customKeys = false;
+
 std::vector<unsigned int> offsets;
 
-// FOV toggles
-int _fowMode = 0;
-int _fowVal = 0;
+uintptr_t fowAddr = NULL;
+uintptr_t mpBoolAddr = NULL;
+uintptr_t patchAddr = NULL;
+int fowMode = 0;
+int fowVal = 0;
 
-// Configuration vars
-bool active = false;
-bool run = true;
-bool cfg = true;
-
-// Get the Process ID
 DWORD GetPID(const PWCHAR pName)
 {
 	DWORD pid = 0;
@@ -56,7 +54,6 @@ DWORD GetPID(const PWCHAR pName)
 	return pid;
 }
 
-// Get the Module Base Address
 PVOID GetMBA(DWORD pid, const PWCHAR mName, DWORD* mSize)
 {
 	PVOID addr = 0;
@@ -82,7 +79,6 @@ PVOID GetMBA(DWORD pid, const PWCHAR mName, DWORD* mSize)
 	return addr;
 }
 
-// Get the addresses from the pointers chain
 uintptr_t ResolveAddrEx(HANDLE hProcess, uintptr_t ptr, std::vector<unsigned int> offsets)
 {
 	uintptr_t addr = ptr;
@@ -94,201 +90,117 @@ uintptr_t ResolveAddrEx(HANDLE hProcess, uintptr_t ptr, std::vector<unsigned int
 	return addr;
 }
 
-void HPX(HANDLE hProcess, PBYTE destination, PBYTE source, SIZE_T size, BYTE* oldBytes)
+void HPX(HANDLE hProcess, PBYTE destination, PBYTE source, SIZE_T size)
 {
 	DWORD oldProtection;
 	VirtualProtectEx(hProcess, destination, size, PAGE_READWRITE, &oldProtection);
-	ReadProcessMemory(hProcess, destination, oldBytes, size, nullptr);
 	WriteProcessMemory(hProcess, destination, source, size, nullptr);
 	VirtualProtectEx(hProcess, destination, size, oldProtection, &oldProtection);
 }
 
-PBYTE Aobs(PCHAR pattern, PCHAR mask, PBYTE begin, SIZE_T size)
-{
-	SIZE_T patternSize = strlen((char*)mask);
-
-	for (int i = 0; i < size; i++)
-	{
-		bool match = true;
-		for (int j = 0; j < patternSize; j++)
-		{
-			if (*(char*)((uintptr_t)begin + i + j) != pattern[j] && mask[j] != '?')
-			{
-				match = false;
-				break;
-			}
-		}
-		if (match) return (begin + i);
-	}
-	return nullptr;
+unsigned int hexToUint(const std::string& hexStr) {
+	unsigned int result;
+	std::stringstream ss;
+	ss << std::hex << hexStr;
+	ss >> result;
+	return result;
 }
 
-char* AobsEx(HANDLE hProc, char* pattern, char* mask, char* begin, intptr_t size)
+bool loadAddresses()
 {
-	char* match{ nullptr };
-	SIZE_T bytesRead;
-	DWORD oldprotect;
-	char* buffer{ nullptr };
-	MEMORY_BASIC_INFORMATION mbi;
-	mbi.RegionSize = 0x1000;
-
-	VirtualQueryEx(hProc, (LPCVOID)begin, &mbi, sizeof(mbi));
-
-	for (char* curr = begin; curr < begin + size; curr += mbi.RegionSize)
-	{
-		if (!VirtualQueryEx(hProc, curr, &mbi, sizeof(mbi))) continue;
-
-		if (mbi.State != MEM_COMMIT || mbi.Protect == PAGE_NOACCESS) continue;
-
-		delete[] buffer;
-		buffer = new char[mbi.RegionSize];
-
-		if (VirtualProtectEx(hProc, mbi.BaseAddress, mbi.RegionSize, PAGE_EXECUTE_READWRITE, &oldprotect))
-		{
-			ReadProcessMemory(hProc, mbi.BaseAddress, buffer, mbi.RegionSize, &bytesRead);
-			VirtualProtectEx(hProc, mbi.BaseAddress, mbi.RegionSize, oldprotect, &oldprotect);
-
-			char* internalAddr = (char*)Aobs(pattern, mask, (PBYTE)buffer, (intptr_t)bytesRead);
-
-			if (internalAddr != nullptr)
-			{
-				match = curr + (internalAddr - buffer);
-				break;
-			}
-		}
-	}
-	delete[] buffer;
-	return match;
-}
-
-// Load offsets from a .txt
-bool loadAddresses(const char* file)
-{
-	std::ifstream inFile;
-	inFile.open(file);
-	if (!inFile) {
+	std::ifstream input_file("data.json");
+	if (!input_file.is_open()) {
+		std::cerr << "data.json doesn't exists!" << std::endl;
 		return false;
 	}
-	else
+
+	json j;
+	input_file >> j;
+
+	std::string gameversion = j["gameversion"];
+	unsigned int patchaddress = hexToUint(j["patchaddress"]);
+	unsigned int fowbase = hexToUint(j["fowaddr"]);
+	unsigned int mpboolbase = hexToUint(j["mpboolbase"]);
+	unsigned int mpboolo1 = hexToUint(j["mpboolo1"]);
+	unsigned int mpboolo2 = hexToUint(j["mpboolo2"]);
+	unsigned int mpboolo3 = hexToUint(j["mpboolo3"]);
+	int cToggleKey = hexToUint(j["togglekey"]);
+	int cExitKey = hexToUint(j["exitkey"]);
+
+	if (cToggleKey != 0)
+		toggleKey = cToggleKey;
+	if (cExitKey != 0)
+		exitKey = cExitKey;
+	if (cToggleKey != 0 || cExitKey != 0)
+		customKeys = true;
+
+	mpBoolAddr = ResolveAddrEx(hProcess, moduleBase + mpboolbase, { mpboolo1, mpboolo2, mpboolo3 });
+	fowAddr = ResolveAddrEx(hProcess, moduleBase + fowbase, { });
+	patchAddr = ResolveAddrEx(hProcess, moduleBase + patchaddress, { });
+
+	SetConsoleTextAttribute(hConsole, 15);
+	std::cout << "MpBool Address: " << std::hex << mpBoolAddr << std::endl;
+	std::cout << "FoW Toggle Address: " << std::hex << fowAddr << std::endl;
+
+	char org[] = "\x90\x90";
+	HPX(hProcess, (PBYTE)patchAddr, (PBYTE)org, 2);
+	patched = true;
+	std::cout << "Patch address: " << std::hex << patchAddr << std::endl;
+
+	if (mpBoolAddr == NULL || fowAddr == NULL)
 	{
-		std::string c;
-		while (inFile >> c);
-		std::string n;
-		int cInserted = 0;
-		int cIndex = 0;
-
-		std::cout << "Offsets file found!" << std::endl;
-
-		// Index =          0       1         2      3   4    5
-		// Example .txt = "51148,127568792,137422160,0,10008,684,"
-		// Split the .txt contents ...
-		for (int i = 0; i < c.size(); i++)
-		{
-			if (c.at(i) == 44) // ... separated with a ","
-			{
-				if (cIndex >= 3) // If >= 3 we are reading offsets
-				{
-					n = c.substr(cInserted, i);
-					offsets.push_back(stoi(n));
-					n.clear();
-					cInserted = i + 1;
-				}
-				else // Else we are reading Game version number / base pointers
-				{
-					n = c.substr(cInserted, i);
-					bases.push_back(stoi(n));
-					n.clear();
-					cInserted = i + 1;
-				}
-				cIndex++;
-			}
-		}
-		std::cout << "Update: " << bases[0] << "." << std::endl;
-
-		return true;
+		SetConsoleTextAttribute(hConsole, 12);
+		std::cerr << "Wrong offsets!" << std::endl;
+		return false;
 	}
-	return false;
-}
 
-bool initPatchAddress()
-{
-	// Patch vma by static offset
-	//_jumppatchaddr = ResolveAddrEx(_hProcess, _moduleBase + 0x50CEB0, { });
+	SetConsoleTextAttribute(hConsole, 14);
+	std::cout << "Addresses loaded. Game Version: " << gameversion << std::endl;
 
-	// Patch by AoB Scan
-	if (!active)
-	{
-		char aob[] = "\x74\x30\x48\x8b\x02\x44\x38\x48";
-		char mask[] = "xxxxxxxx";
-		_jumppatchaddr = (uintptr_t)AobsEx(_hProcess, (PCHAR)aob, (PCHAR)mask, (char*)_moduleBase, (intptr_t)_mSize);
-	}
-	else
-	{
-		char aob[] = "\xeb\x35\x48\x8b\x02\x44\x38\x48";
-		char mask[] = "xxxxxxxx";
-		_jumppatchaddr = (uintptr_t)AobsEx(_hProcess, (PCHAR)aob, (PCHAR)mask, (char*)_moduleBase, (intptr_t)_mSize);
-	}
-	if (!_jumppatchaddr) return false;
 	return true;
 }
-
-// Load offsets at Runtime if cfg = true (Use offsets.txt file)
-bool initAddressesCFG()
+void toggleOn()
 {
-	_fowtoggleaddr = ResolveAddrEx(_hProcess, _moduleBase + bases[2], offsets);
-	_fowmodeaddr = ResolveAddrEx(_hProcess, _moduleBase + bases[1], { });
-	if (!initPatchAddress()) return false;
-	return true;
+	active = true;
+	ReadProcessMemory(hProcess, (int*)fowAddr, &fowVal, sizeof(fowVal), NULL);
+	ReadProcessMemory(hProcess, (int*)mpBoolAddr, &fowMode, sizeof(fowMode), NULL);
+	fowVal += 1;
+	fowMode += 1;
+	WriteProcessMemory(hProcess, (int*)fowAddr, &fowVal, sizeof(fowVal), NULL);
+	WriteProcessMemory(hProcess, (int*)mpBoolAddr, &fowMode, sizeof(fowMode), NULL);
 }
 
-// Load offsets at Runtime if cfg = false (Use hard coded offsets)
-bool initAddresses()
+void toggleOff()
 {
-	_fowtoggleaddr = ResolveAddrEx(_hProcess, _moduleBase + 0x84EE578, { 0x0, 0x2718, 0x2C8 });
-	_fowmodeaddr = ResolveAddrEx(_hProcess, _moduleBase + 0x7B82C28, { });
-	if (!initPatchAddress()) return false;
-	return true;
+	active = false;
+	ReadProcessMemory(hProcess, (int*)fowAddr, &fowVal, sizeof(fowVal), NULL);
+	ReadProcessMemory(hProcess, (int*)mpBoolAddr, &fowMode, sizeof(fowMode), NULL);
+	fowVal -= 1;
+	fowMode -= 1;
+	WriteProcessMemory(hProcess, (int*)fowAddr, &fowVal, sizeof(fowVal), NULL);
+	WriteProcessMemory(hProcess, (int*)mpBoolAddr, &fowMode, sizeof(fowMode), NULL);
 }
-
-// Main Loop
 void input()
 {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	run = true;
 
 	while (run)
 	{
-		if (GetAsyncKeyState(0x50) & 1) // P Key
+		if (GetAsyncKeyState(exitKey) & 1) // End Key
 		{
-			bool addrReady = true;
-			if (cfg)
-			{
-				addrReady = initAddressesCFG();
-			}
-			else
-			{
-				addrReady = initAddresses();
-			}
+			run = false;
+			SetConsoleTextAttribute(hConsole, 10);
+			std::cout << "Exiting..." << std::endl;
+			if (active)
+				toggleOff();
+			SetConsoleTextAttribute(hConsole, 15);
+			continue;
+		}
+		if (GetAsyncKeyState(toggleKey) & 1) // P Key
+		{
 
-			if (!addrReady)
-			{
-				SetConsoleTextAttribute(hConsole, 12);
-				std::cout << "ERROR: COULDN'T FIND SIGNATURE! PLEASE UPDATE." << std::endl;
-				continue;
-			}
-
-			ReadProcessMemory(_hProcess, (int*)_fowtoggleaddr, &_fowVal, sizeof(float), NULL);
 			if (!active) {
-				active = true;
-				ReadProcessMemory(_hProcess, (int*)_fowtoggleaddr, &_fowVal, sizeof(_fowVal), NULL);
-				ReadProcessMemory(_hProcess, (int*)_fowmodeaddr, &_fowMode, sizeof(_fowMode), NULL);
-				_fowVal += 1;
-				_fowMode += 1;
-				WriteProcessMemory(_hProcess, (int*)_fowmodeaddr, &_fowMode, sizeof(_fowMode), NULL);
-				WriteProcessMemory(_hProcess, (int*)_fowtoggleaddr, &_fowVal, sizeof(_fowVal), NULL);
-
-				// Unconditional Jump to skip the FoW check
-				char org[] = "\xeb\x35";
-				HPX(_hProcess, (PBYTE)_jumppatchaddr, (PBYTE)org, 2, buffer);
+				toggleOn();
 
 				SetConsoleTextAttribute(hConsole, 15);
 				std::cout << "Anti FoW ";
@@ -296,17 +208,7 @@ void input()
 				std::cout << "ON." << std::endl;
 			}
 			else {
-				active = false;
-				ReadProcessMemory(_hProcess, (int*)_fowtoggleaddr, &_fowVal, sizeof(_fowVal), NULL);
-				ReadProcessMemory(_hProcess, (int*)_fowmodeaddr, &_fowMode, sizeof(_fowMode), NULL);
-				_fowVal -= 1;
-				_fowMode -= 1; 
-				WriteProcessMemory(_hProcess, (int*)_fowmodeaddr, &_fowMode, sizeof(_fowMode), NULL);
-				WriteProcessMemory(_hProcess, (int*)_fowtoggleaddr, &_fowVal, sizeof(_fowVal), NULL);
-
-				// Restore Jump if Zero FoW check
-				char org[] = "\x74\x30";
-				HPX(_hProcess, (PBYTE)_jumppatchaddr, (PBYTE)org, 2, buffer);
+				toggleOff();
 
 				SetConsoleTextAttribute(hConsole, 15);
 				std::cout << "Anti FoW ";
@@ -322,35 +224,53 @@ void input()
 
 int main()
 {
-	// We look for the file "offsets.txt"
-
-	cfg = loadAddresses("offsets.txt");
-
-	// In not .txt file found at all, use the hard coded offsets
-	if (!cfg) {
-		std::cout << "Update: 10.1.48." << std::endl;
-	}
 	DWORD pid = 0;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 15);
 
-	// Find the Process ID RelicCardinal.exe for Steam version
 	pid = GetPID((PWCHAR)L"RelicCardinal.exe");
 
 	if (pid != 0)
 	{
-		// Find the Module Base Address
-		_hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, pid);
-		_moduleBase = (uintptr_t)GetMBA(pid, (PWCHAR)L"RelicCardinal.exe", &_mSize);
-
-		buffer = new BYTE[5]; // Buffer for FoW values (old bytes from patching binary)
-
-		// Extra prints
-		std::cout << "God Hates Cheats!" << std::endl;
-		std::cout << "Toggle Key: \"P\"" << std::endl;
-		input();
+		hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, pid);
+		moduleBase = (uintptr_t)GetMBA(pid, (PWCHAR)L"RelicCardinal.exe", &mSize);
 	}
 	else {
+		SetConsoleTextAttribute(hConsole, 12);
 		std::cout << "Game not found. Press Enter to exit." << std::endl;
 		char ctest = getchar();
+		return 1;
 	}
+
+	if (!loadAddresses())
+	{
+		SetConsoleTextAttribute(hConsole, 12);
+		std::cout << "Failed to load addresses. Press Enter to exit." << std::endl;
+		char ctest = getchar();
+		return 1;
+	}
+
+	SetConsoleTextAttribute(hConsole, 10);
+	std::cout << "God Hates Cheats!" << std::endl;
+	SetConsoleTextAttribute(hConsole, 14);
+	if (customKeys)
+	{
+		std::cout << "Custom keys loaded." << std::endl;
+		std::cout << "Toggle Key: 0x" << std::hex << toggleKey << std::endl;
+		std::cout << "Exit Key: 0x" << std::hex << exitKey << std::endl;
+	}
+	else
+	{
+		std::cout << "Default keys loaded." << std::endl;
+		std::cout << "Toggle Key: \"P\"" << std::endl;
+		std::cout << "Exit Key: \"End\"" << std::endl;
+	}
+	input();
+	if (patched)
+	{
+		char org[] = "\x7D\x16";
+		HPX(hProcess, (PBYTE)patchAddr, (PBYTE)org, 2);
+	}
+
 	return 0;
 }
